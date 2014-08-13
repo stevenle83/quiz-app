5868369d82b8ce1c36407481d5bef6c541405539
Steven Le <lesteven1983@gmail.com>
2014-08-13 05:47:51 -0500

    quizz-app
    get started

---
+++ js/apps.js
@@ -0,0 +1,22 @@
+$(document).ready(function() { 
+
+	$('#startBtn').click(function (event) {
+		event.preventDefault();
+		$('.intro').hide();
+		$('.question1').show();
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
