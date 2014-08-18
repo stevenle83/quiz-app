aff294819bfe236d0404a7094c8030c264b29564
Steven Le <lesteven1983@gmail.com>
2014-08-18 16:16:05 -0500

    quiz-app
    more work to be done!!!

--- js/apps.js
+++ js/apps.js
@@ -1,54 +1,54 @@
 $(document).ready(function() { 
 
-	/*---Questions Array---*/
+	//Questions Array
 
 	var questions = [
 
 		{
-			question: "The Walking Dead series take place in which state?",
-			choices: ["New York", "Idaho", "Georgia", "Texas"],
-			questNum: "1.",
+			question: "After the attack on the prison which casused the group's seperation. Glenn returns and finds who?",
+			choices: ["Lizzie", "Lilly", "Tara", "Rosita"],
+			questNum: 1,
 			correct: 2,
 			fact: ""
 
 		},
 
 		{
-			question: "When the zombie apocalypse started, where was Rick?",
-			choices: ["in the hospital", "with his family", "in Atlanta", "outside killing zombies"],
-			questNum: "2. ",
-			correct: 0,
+			question: "Why did the Governor want to kill Michonne?",
+			choices: ["He was jealous of her relationship with Andrea", "She didn't kill Rick.", "She killed his henchmen.", "She killed his daughter, who was already a zombie."],
+			questNum: 2,
+			correct: 3,
 			fact: ""
 		},
 
 		{
-			question: "Which character chose to stay back at the CDC to die from the facility's explosion",
-			choices: ["Merle", "Jim", "Amy", "Jacqui"],
-			questNum: "3. ",
+			question: "Which character chose to stay back at the CDC to die from the facility's explosion?",
+			choices: ["Patricia", "Mika", "Amy", "Jacqui"],
+			questNum: 3,
 			correct: 3,
 			fact: ""
 		},
 
 		{
 			question: "Why did Otis accidentally shoot Carl?",
-			choices: ["he thought Carl was a walker", "Carl was annoying", "he was shooting a deer", "Carl was next to a walker"],
-			questNum: "4. ",
+			choices: ["He thought Carl was a walker.", "Carl was annoying.", "He was shooting a deer.", "Carl was next to a walker."],
+			questNum: 4,
 			correct: 2,
 			fact: ""
 		},
 
 		{
 			question: "Which two characters' deaths were caused by Shane?",
-			choices: ["Otis & Randall", "Otis & John", "Dale & Jimmy", "Jimmy & Patricia"],
-			questNum: "5. ",
+			choices: ["Otis & Randall", "Otis & Dale", "Otis & Jimmy", "Otis & Patricia"],
+			questNum: 5,
 			correct: 0,
 			fact: ""
 		},
 
 		{
-			question: "Who was bitten in the neck at the prison and sacrificed his/her life to save Carol?",
-			choices: ["Oscar", "Axel", "Lori", "T-Dog"],
-			questNum: "6. ",
+			question: "Which character did Carol kill?",
+			choices: ["Karen", "Lizzie", "David", "All of the above."],
+			questNum: 6,
 			correct: 3,
 			fact: ""
 		},
@@ -56,15 +56,15 @@ $(document).ready(function() {
 		{
 			question: "When Rick, Michonne, and Carl returned to Rick's hometown, they ran into an old friend from season one. Who was he?",
 			choices: ["Duane", "The Governor", "Morgan", "Shane"],
-			questNum: "7. ",
+			questNum: 7,
 			correct: 2,
 			fact: ""
 		},
 
 		{
 			question: "How did Hershel die?",
-			choices: ["he was decapitated", "he was bitten by a walker", "he was shot", "he lost another leg"],
-			questNum: "8. ",
+			choices: ["He was decapitated.", "He was bitten by a walker.", "He was shot.", "He lost another leg."],
+			questNum: 8,
 			correct: 0,
 			fact: ""
 		},
@@ -72,15 +72,15 @@ $(document).ready(function() {
 		{
 			question: "Who killed the Governor?",
 			choices: ["Rick", "Maggie", "Michonne", "Lilly"],
-			questNum: "9. ",
+			questNum: 9,
 			correct: 3,
 			fact: ""
 		},
 
 		{
 			question: "What do the people of Woodbury call the zombies?",
-			choices: ["scary zombies", "walkers", "biters", "geeks"],
-			questNum: "10. ",
+			choices: ["the dead", "walkers", "biters", "geeks"],
+			questNum: 10,
 			correct: 2,
 			fact: ""
 		}
@@ -91,6 +91,8 @@ $(document).ready(function() {
 	var currentQuestion = 0;
 	var numCorrect = 0;
 
+	/*---Functions Declaration---*/
+
 	//Play Intro Theme Function
 	var playIntro = function () {
 
@@ -100,7 +102,7 @@ $(document).ready(function() {
 
 	}
 
-	//Fade In Jumbotron Intro
+	//Fade In Jumbotron/Intro Function
 	var fadeIntro = function () {
 
 		$('.intro').fadeIn(5000);	
@@ -114,34 +116,60 @@ $(document).ready(function() {
 
 	}
 
-	//Submit Answer Function
-	var submitAnswer = function () {
+	//Display Current Score Function
+	var displayScore = function () {
 
-		var answer = $("input[type='radio']:checked").val();
-		
-		if ( answer == questions[currentQuestion].correct ) {
-			alert("Correct!!!");
-			numCorrect ++;
-		}
+		var score = numCorrect + " out of 10 correct.";
 
-		else {
-			alert("Wrong!!!");
-		}
+		$('#score').html(score);
 
-		currentQuestion ++;
-		nextQuestion();
+	}
+
+	//Display Progress Function
+	var progress = function () {
+
+		var progress = "You are on question #" + questions[currentQuestion].questNum + " out of 10." 
+
+		$('#progress').html(progress);
 
 	}
 
 	//Next Question Function
 	var nextQuestion = function () {
 
-		var newQuestion = '<h2>' + questions[currentQuestion].questNum + questions[currentQuestion].question + '</h2>';
+		var newQuestion = '<h2>' + questions[currentQuestion].question + '</h2>';
 		var newChoices = '<input type="radio" name="option" value="0"> ' + questions[currentQuestion].choices[0] + '<br><input type="radio" name="option" value="1"> ' + questions[currentQuestion].choices[1] + '<br><input type="radio" name="option" value="2"> ' + questions[currentQuestion].choices[2] + '<br><input type="radio" name="option" value="3"> ' + questions[currentQuestion].choices[3];
 		$('.question-wrapper').html(newQuestion);
 		$('.answer-wrapper').html(newChoices);
 
 	}
+
+	//Submit Answer Function
+	var submitAnswer = function () {
+
+		var answer = $("input[type='radio']:checked").val();
+
+		if ( answer == questions[currentQuestion].correct ) {
+
+			numCorrect ++;
+			$('#stat').html("You answered correctly!!!");
+
+		} 
+
+		else {
+
+			$('#stat').html("You answered incorrectly!!!");
+
+		} 	
+
+		currentQuestion ++;
+		progress();
+		displayScore();
+		nextQuestion();
+
+	}
+
+	/*---Calling Functons---*/
 
 	//Play Intro Theme
 	playIntro();
