ad7827189d12981aad85ad74b2d67d2ce92039d7
Steven Le <lesteven1983@gmail.com>
2014-08-20 10:24:43 -0500

    quiz-app
    finally done

--- js/apps.js
+++ js/apps.js
@@ -1,88 +1,84 @@
 $(document).ready(function() { 
 
-	/*---Questions Array---*/
+	//Questions Array
 
 	var questions = [
 
 		{
-			question: "The Walking Dead series take place in which state?",
-			choices: ["New York", "Idaho", "Georgia", "Texas"],
-			questNum: 0,
-			correct: 2,
-			fact: ""
-
-		},
-
-		{
-			question: "When the zombie apocalypse started, where was Rick?",
-			choices: ["in the hospital", "with his family", "in Atlanta", "outside killing zombies"],
+			question: "After the attack on the prison which caused the group's seperation, Glenn returns to the prison and finds who?",
+			choices: ["Lizzie", "Lilly", "Tara", "Rosita"],
 			questNum: 1,
-			correct: 0,
-			fact: ""
+			correct: 2
 		},
 
 		{
-			question: "Which character chose to stay back at the CDC to die from the facility's explosion",
-			choices: ["Merle", "Jim", "Amy", "Jacqui"],
+			question: "Why did the Governor want to kill Michonne?",
+			choices: ["He was jealous of her relationship with Andrea", "She didn't kill Rick.", "She killed his henchmen.", "She killed his zombie daughter, Penny."],
 			questNum: 2,
-			correct: 3,
-			fact: ""
+			correct: 3
+		},
+
+		{
+			question: "After everyone left the prison, where did they finally reunite?",
+			choices: ["The Hideout", "The Terminus", "The Terminal", "Woodbury"],
+			questNum: 3,
+			correct: 1
 		},
 
 		{
 			question: "Why did Otis accidentally shoot Carl?",
-			choices: ["he thought Carl was a walker", "Carl was annoying", "he was shooting a deer", "Carl was next to a walker"],
-			questNum: 3,
-			correct: 2,
-			fact: ""
+			choices: ["He thought Carl was a walker.", "Carl was annoying.", "He was shooting a deer.", "Carl was next to a walker."],
+			questNum: 4,
+			correct: 2
 		},
 
 		{
 			question: "Which two characters' deaths were caused by Shane?",
-			choices: ["Otis & Randall", "Otis & John", "Dale & Jimmy", "Jimmy & Patricia"],
-			questNum: 4,
-			correct: 0,
-			fact: ""
-		},
-
-		{
-			question: "Who was bitten in the neck at the prison and sacrificed his/her life to save Carol?",
-			choices: ["Oscar", "Axel", "Lori", "T-Dog"],
+			choices: ["Otis & Randall", "Otis & Dale", "Otis & Jimmy", "Otis & Patricia"],
 			questNum: 5,
-			correct: 3,
-			fact: ""
+			correct: 0
 		},
 
 		{
-			question: "When Rick, Michonne, and Carl returned to Rick's hometown, they ran into an old friend from season one. Who was he?",
-			choices: ["Duane", "The Governor", "Morgan", "Shane"],
+			question: "Who was killed by Carol?",
+			choices: ["Karen", "Lizzie", "David", "All of the above."],
 			questNum: 6,
-			correct: 2,
-			fact: ""
+			correct: 3
+		},
+
+		{
+			question: "When Rick, Michonne, and Carl returned to Rick's hometown, they ran into an old friend. Who was he?",
+			choices: ["Duane", "The Governor", "Morgan", "Shane"],
+			questNum: 7,
+			correct: 2
 		},
 
 		{
 			question: "How did Hershel die?",
-			choices: ["he was decapitated", "he was bitten by a walker", "he was shot", "he lost another leg"],
-			questNum: 7,
-			correct: 0,
-			fact: ""
+			choices: ["He was decapitated.", "He was bitten by a walker.", "He was shot.", "He lost another leg."],
+			questNum: 8,
+			correct: 0
 		},
 
 		{
 			question: "Who killed the Governor?",
 			choices: ["Rick", "Maggie", "Michonne", "Lilly"],
-			questNum: 8,
-			correct: 3,
-			fact: ""
+			questNum: 9,
+			correct: 3
 		},
 
 		{
 			question: "What do the people of Woodbury call the zombies?",
-			choices: ["scary zombies", "walkers", "biters", "geeks"],
-			questNum: 9,
-			correct: 3,
-			fact: ""
+			choices: ["the dead", "walkers", "biters", "geeks"],
+			questNum: 10,
+			correct: 2
+		},
+
+		{
+			question: "",
+			choices:"",
+			questNum: "",
+			correct:""
 		}
 
 	]
@@ -90,6 +86,8 @@ $(document).ready(function() {
 	//Global Variables
 	var currentQuestion = 0;
 	var numCorrect = 0;
+
+	/*---Functions Declaration---*/
 
 	//Play Intro Theme Function
 	var playIntro = function () {
@@ -100,7 +98,7 @@ $(document).ready(function() {
 
 	}
 
-	//Fade Page In Intro
+	//Fade In Jumbotron/Intro Function
 	var fadeIntro = function () {
 
 		$('.intro').fadeIn(5000);	
@@ -114,11 +112,61 @@ $(document).ready(function() {
 
 	}
 
-	//Submit Answer Function
-	var submitAnswer = function () {
+	//Rank Results Function
+	var rank = function () {
 
-		var answer = $("input[type='radio']:checked").val();
-		alert(answer);
+		if ( numCorrect <= 2 ) {
+
+			$('#result').html("You must not have cable!");
+
+		} else if ( numCorrect > 2 && numCorrect <= 4 ) {
+
+			$('#result').html("You are a Walkind Dead Noobie!");
+
+		} else if ( numCorrect > 4 && numCorrect <= 6 ) {
+
+			$('#result').html("You must watch a lot of Walkding Dead!");
+
+		} else if ( numCorrect > 6 && numCorrect <= 8 ) {
+
+			$('#result').html("You know your Walkind Dead!");
+
+		} else {
+
+			$('#result').html("You watch too much Walking Dead!");
+			
+		}
+
+	}
+
+	//Display Current Score Function
+	var displayScore = function () {
+
+		var score = "Score: " + numCorrect + " Out Of 10 Correct.";
+
+		$('#score').html(score);
+
+	}
+
+	//Display Progress Function
+	var progress = function () {
+
+		var progress = "Question Number " + questions[currentQuestion].questNum + " Out Of 10." 
+
+		$('#progress').html(progress);
+
+	}
+
+	//New Game Function
+	var newGame = function () {
+
+		numCorrect = 0;
+		currentQuestion = 0;
+		nextQuestion();
+		displayScore();
+		progress();
+		$('#submit').show();
+		$('#result').html("Result:");
 
 	}
 
@@ -127,11 +175,51 @@ $(document).ready(function() {
 
 		var newQuestion = '<h2>' + questions[currentQuestion].question + '</h2>';
 		var newChoices = '<input type="radio" name="option" value="0"> ' + questions[currentQuestion].choices[0] + '<br><input type="radio" name="option" value="1"> ' + questions[currentQuestion].choices[1] + '<br><input type="radio" name="option" value="2"> ' + questions[currentQuestion].choices[2] + '<br><input type="radio" name="option" value="3"> ' + questions[currentQuestion].choices[3];
+
 		$('.question-wrapper').html(newQuestion);
-		$('.answer-wrapper .row .col-xs-12').html(newChoices);
+		$('.answer-wrapper').html(newChoices);
+
+		if ( currentQuestion == 10 ) {
+
+			$('.answer-wrapper').html("Thanks For Playing.<br><br><br>Review Your Results.<br><br><br>Click New Game To Start Over.");
+			$('#submit').hide();
+			$('#progress').html("You finished all 10 questions.");
+			rank();
+
+		}
 
 	}
 
+	//Submit Answer Function
+	var submitAnswer = function () {
+
+		var answer = $("input[type='radio']:checked").val();
+		
+		if ( answer == questions[currentQuestion].correct ) {
+
+			numCorrect ++;
+			$('#result').html("Result: Correct!!!");
+
+		} else {
+
+			$('#result').html("Result: Incorrect!!!");
+
+		} 
+
+		currentQuestion ++;
+		progress();
+		displayScore();
+		nextQuestion();
+
+	}
+
+	/*---Calling Functons---*/
+
+	//Play Intro Theme
+	playIntro();
+
+	//Fade In Jumbotron Intro	
+	fadeIntro();	
 
 	//Start Quiz
 	$('.startBtn').click(function () {
@@ -147,27 +235,11 @@ $(document).ready(function() {
 
 	});
 
-	//Play Intro Theme
-	playIntro();
+	//New Game
+	$('#new').click(function () {
 
-	//Fade In Jumbotron Intro	
-	fadeIntro();	
+		newGame();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	});
 
 });
